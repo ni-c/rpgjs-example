f73faed919dc165c72651f8a8da46cbc5349baea
ni-c <willi@roamsys.com>
2012-12-25 22:49:01 +0100

    Bugfixing

--- app.js
+++ app.js
@@ -8,20 +8,21 @@ requirejs.config({
 });
 
 requirejs(['./rpgjs'], function(rpgjs) {
-rpgjs.onConnect(1337, function(rpg) {
-  rpg.start(function() {
-		rpg.loadMap('map', {
-			tileset: 'grassland.png',
-			autotiles: ['grassland1.png','grassland2.png','grassland3.png','grassland4.png','grassland5.png','grassland6.png','grassland7.png'],
-			player:  {
-				x: 12, 
-				y: 8, 
-				filename: 'fighter.png'
-			}
-	  }, function() {
-	
-	 	});
+	rpgjs.onConnect(1337, function(rpg) {
+	  rpg.start(function() {
+			rpg.loadMap('map', {
+				tileset: 'grassland.png',
+				autotiles: ['grassland1.png','grassland2.png','grassland3.png','grassland4.png'],
+				player:  {
+		  		speedAnimation: 10,
+	  			speed: 10,
+					x: 12, 
+					y: 8, 
+					filename: 'fighter.png'
+				}
+		  }, function() {
+		
+		 	});
+		});
 	});
-});
-
 });
