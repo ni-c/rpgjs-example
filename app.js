a7b417ea953ac7b3e850a85259bd2a61708fef9e
ni-c <willi@roamsys.com>
2012-12-25 19:47:43 +0100

    Finished first runable version

--- app.js
+++ app.js
@@ -9,20 +9,19 @@ requirejs.config({
 
 requirejs(['./rpgjs'], function(rpgjs) {
 rpgjs.onConnect(1337, function(rpg) {
-
   rpg.start(function() {
-
-    rpg.loadMap('map', {
-      tileset : 'tiles.png',
-      player : {
-        x : 2,
-        y : 3,
-        filename : 'Hero.png'
-      }
-    }, function() {
-
-    });
-  });
+		rpg.loadMap('map', {
+			tileset: 'grassland.png',
+			autotiles: ['grassland1.png','grassland2.png','grassland3.png','grassland4.png','grassland5.png','grassland6.png','grassland7.png'],
+			player:  {
+				x: 12, 
+				y: 8, 
+				filename: 'fighter.png'
+			}
+	  }, function() {
+	
+	 	});
+	});
 });
 
 });
